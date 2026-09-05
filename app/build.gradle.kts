94528f1cb51a258fb74fea2158c62beb29f7431f
Alexis Svinartchouk <zvin@free.fr>
2026-09-05 16:20:23 +0400

    optimize release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,11 +43,9 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            optimization {
+                enable = true
+            }
         }
     }
     compileOptions {
