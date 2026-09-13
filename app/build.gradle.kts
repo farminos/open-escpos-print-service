3210e7dfb61ec792913d7343b9bce13481d508a4
Alexis Svinartchouk <zvin@free.fr>
2026-09-13 13:14:34 +0400

    v1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.farminos.print"
         minSdk = 23
         targetSdk = 37
-        versionCode = 30
-        versionName = "1.4.0"
+        versionCode = 31
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
